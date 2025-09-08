d683d6facfb0096779b0a48dfd833f6441ff74a9
naveed arshad <nav.shad1756@gmail.com>
2025-09-08 21:27:25 +0530

    worked on backend
    added map util features to geoutils.js
    
    cleaned maptesting.html
    
    removed map.js

--- src/scripts/backend/geoutils.js
+++ src/scripts/backend/geoutils.js
@@ -2,7 +2,6 @@
 function get_coords(tryls = false, putls = false, callback = function(coords, status) {console.log(coords, status)}) {
     if (tryls) {
         const coords = localStorage.getItem("geocoords")
-        console.log(coords)
         if (coords) {
             callback(JSON.parse(coords), 1)
             return
@@ -24,7 +23,9 @@ function get_coords(tryls = false, putls = false, callback = function(coords, st
 
 function get_address(latitude, longtitude, callback = function(address, status) {console.log(address, status)}) {
     fetch(`http://localhost:3000/app/utils/locationdat?lat=${latitude}&lon=${longtitude}`)
-    .then(res => res.json())
+    .then(function(res) {
+        return res.json()
+    })
     .then(address => {
         callback(address, 1)
     })
@@ -33,4 +34,27 @@ function get_address(latitude, longtitude, callback = function(address, status) 
     })
 }
 
-get_coords(true, true)
+
+//assuming leaflet.js is loaded
+
+function get_map(display_element_id) {
+    var map = L.map("map").setView([20.5937, 78.9629], 5)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 19}).addTo(map)
+    return map
+}
+
+function add_marker_on_map(coords, map) {
+    var marker = L.marker(coords).addTo(map)
+    return marker
+}
+
+function remove_marker_on_map(map, marker) {
+    map.removeLayer(marker)
+}
+
+function set_map_markevent(map, callback = function(coords) {console.log(coords)}) {
+    map.on("click", function(event) {
+        const {lat, lng} = event.latlng
+        callback([lat, lng])
+    })
+}
